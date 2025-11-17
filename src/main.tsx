5de417594ef117ee92126d837c971adf23db3e8a
colton615-coder <colton615@gmail.com>
2025-11-17 04:22:45 +0000

    Generated by Spark: Restore affirmations on load up screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,31 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
+import { useState } from 'react'
 import "@github/spark/spark"
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 import { ThemeProvider } from './components/ThemeProvider.tsx'
+import { LoadingScreen } from './components/LoadingScreen.tsx'
 
 import "./main.css"
 import "./styles/theme.css"
 import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <ThemeProvider defaultTheme="system">
-      <App />
-    </ThemeProvider>
-  </ErrorBoundary>
-)
+function Root() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ThemeProvider defaultTheme="system">
+        {isLoading ? (
+          <LoadingScreen onLoadComplete={() => setIsLoading(false)} />
+        ) : (
+          <App />
+        )}
+      </ThemeProvider>
+    </ErrorBoundary>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<Root />)
